a6a7cec360e7210fea6bf73e94a987ecdaccdf1d
nrproject182-cmd <nrproject182@gmail.com>
2026-08-26 23:50:08 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* NR Edit Pro — Service Worker v1.1.0 */
-var VERSION = '1.1.0';
+/* NR Edit Pro — Service Worker v1.1.1 */
+var VERSION = '1.1.1';
 var CACHE = 'nr-edit-pro-' + VERSION;
 var CORE = ['./', './index.html'];
 self.addEventListener('install', function (e) {
